b79d2f272567c5eb79a5e492922c152c08dda53f
Luis Araujo <c.araujo@pucp.edu.pe>
2026-05-07 20:07:27 -0500

    develop

---
+++ src/test/java/com/example/lab5_20207779/Lab520207779ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab5_20207779;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab520207779ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
